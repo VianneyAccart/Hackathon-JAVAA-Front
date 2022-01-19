9f0f675af5ee7be4decc9428cb2fe908989ead2d
Vianney Accart <vianneyaccart@gmail.com>
2022-01-19 23:26:04 +0100

    adjust css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './header/header.component';
-
 import { SearchPageComponent } from './search-page/search-page.component';
 import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
@@ -15,14 +13,12 @@ import { CreateProjectComponent } from './create-project/create-project.componen
 @NgModule({
   declarations: [
     AppComponent,
-
     HomepageComponent,
     HeaderComponent,
     SearchPageComponent,
     ProjectOverviewComponent,
     CreateProjectComponent,
     ProjectOverviewComponent
-
   ],
   imports: [
     BrowserModule,
