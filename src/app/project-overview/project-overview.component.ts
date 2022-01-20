abf36e14b3398c016a67dba315b3859816273b8b
Vianney Accart <vianneyaccart@gmail.com>
2022-01-20 15:24:23 +0100

    realize project service and map datas, improve css of cards

--- src/app/project-overview/project-overview.component.ts
+++ src/app/project-overview/project-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { imagesUrl } from 'src/environments/environment';
+import { ProjectService } from '../shared/services/project.service';
 
 @Component({
   selector: 'app-project-overview',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectOverviewComponent implements OnInit {
 
-  constructor() { }
+  projects : any;
+  imageUrl: string = imagesUrl;
 
-  ngOnInit(): void {
+  constructor(private projectsService: ProjectService) { 
   }
 
+  ngOnInit(): void {
+    this.projectsService.getProjects().subscribe(response => {
+      this.projects = response;
+      console.log(this.projects);
+    });
+  }
 }
---
+++ src/app/shared/services/project.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  baseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = 'http://localhost:8080/';
+  }
+
+  public getProjects(): Observable<any> {
+    return this.http.get<any>(this.baseUrl + 'projects');
+  }
+}
