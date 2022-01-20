ad6ebe34973e1a7f0e23c296aaeff0861801b59a
Vianney Accart <vianneyaccart@gmail.com>
2022-01-20 11:37:42 +0100

    create product model and create fake datas into product-overview.ts

--- src/app/shared/model/professional.ts
+++ src/app/shared/model/professional.ts
@@ -2,77 +2,17 @@ export class Professional {
     
   private _id: number;
   private _name: string;
-  private _activity: string;
-  private _picture: any;
+  private _activity: string; // TODO: Set right type
+  private _image: string;
+  private _description : string;
 
-  constructor(id: number, name: string, activity: string, picture: any) {
+  constructor(id: number, name: string, activity: string, image: string, description: string) {
     this._id = id;
     this._name = name;
     this._activity = activity;
-    this._picture = picture;
+    this._image = image;
+    this._description = description;
   }
 
-  /**
-   * Getter id
-   * @return {number}
-   */
-  public get id(): number {
-    return this._id;
-  }
-
-  /**
-   * Getter name
-   * @return {string}
-   */
-  public get name(): string {
-    return this._name;
-  }
-
-  /**
-   * Getter activity
-   * @return {string}
-   */
-  public get activity(): string {
-    return this._activity;
-  }
-
-  /**
-   * Getter picture
-   * @return {any}
-   */
-  public get picture(): any {
-    return this._picture;
-  }
-
-  /**
-   * Setter id
-   * @param {number} value
-   */
-  public set id(value: number) {
-    this._id = value;
-  }
-
-  /**
-   * Setter name
-   * @param {string} value
-   */
-  public set name(value: string) {
-    this._name = value;
-  }
-
-  /**
-   * Setter activity
-   * @param {string} value
-   */
-  public set activity(value: string) {
-    this._activity = value;
-  }
-
-  /**
-   * Setter picture
-   * @param {any} value
-   */
-  public set picture(value: any) {
-    this._picture = value;
-  }
+  // TODO : Add getters/setters
 }
