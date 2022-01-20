01b7e47cb6146c2cf2476d886d094c011b7c5342
Vianney Accart <vianneyaccart@gmail.com>
2022-01-20 10:38:41 +0100

    create interpolation for blog-post, add toggling new search panel

--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostComponent implements OnInit {
 
+  blogPost: any = [
+    {
+      image : "https://cdn.radiofrance.fr/s3/cruiser-production/2020/08/f76e923a-0072-4c0d-9837-7bf89d315133/1136_les_chats.jpg",
+      title : "Salle de bain design : moderne et fonctionnelle Salle de bain design : moderne et fonctionnelle",
+      description : "Vous rêvez d’une salle de bains design ? Dans ce cas, vous êtes prêts à bouleverser les codes et à envisager l’espace autrement. Formes des meubles, disposition...",
+      url : "https://chop-ton-job.web.app"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
